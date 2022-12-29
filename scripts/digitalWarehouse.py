3422831d2640986e6079ec559712f3e181084857
filipeRpereira <a12560@alunos.ipca.pt>
2022-12-29 13:22:28 +0000

    0.0.1

--- scripts/digitalWarehouse.py
+++ scripts/digitalWarehouse.py
@@ -52,7 +52,14 @@ import rospy
 import moveit_commander
 import moveit_msgs.msg
 import geometry_msgs.msg
+import actionlib
+
 from sensor_msgs.msg import JointState
+from control_msgs.msg import GripperCommandAction
+from trajectory_msgs.msg import JointTrajectoryPoint
+from moveit_msgs.msg import Grasp, PickupAction, PickupGoal, PickupResult
+from geometry_msgs.msg import Pose, PoseStamped, PoseArray, Vector3Stamped, Vector3, Quaternion, Point
+from moveit_msgs.msg import MoveItErrorCodes
 import numpy as np
 from geometry_msgs.msg import Quaternion
 
@@ -66,6 +73,12 @@ except:  # For Python 2 compatibility
     def dist(p, q):
         return sqrt(sum((p_i - q_i) ** 2.0 for p_i, q_i in zip(p, q)))
 
+moveit_error_dict = {}
+for name in MoveItErrorCodes.__dict__.keys():
+    if not name[:1] == '_':
+        code = MoveItErrorCodes.__dict__[name]
+        moveit_error_dict[code] = name
+
 
 from std_msgs.msg import String
 from moveit_commander.conversions import pose_to_list
@@ -74,25 +87,10 @@ from moveit_commander.conversions import pose_to_list
 moveit_commander.roscpp_initialize(sys.argv)
 rospy.init_node("move_group_python_interface_tutorial", anonymous=True)
 
-
 pub = rospy.Publisher("/joint_command", JointState, queue_size=20)
 joint_command_isaac = JointState()
 
-'''
-joint_command_isaac.name = [
-"panda_joint1",
-"panda_joint2",
-"panda_joint3",
-"panda_joint4",
-"panda_joint5",
-"panda_joint6",
-"panda_joint7",
-"panda_finger_joint1",
-"panda_finger_joint2",
-]
-'''
-
-
+pub_gripper = rospy.Publisher("/gripper_command", Grasp,queue_size=20)
 
 positions = []
 velocities = []
@@ -135,13 +133,6 @@ class MoveGroupPythonInterfaceTutorial(object):
     def __init__(self):
         super(MoveGroupPythonInterfaceTutorial, self).__init__()
 
-        ## BEGIN_SUB_TUTORIAL setup
-        ##
-        ## First initialize `moveit_commander`_ and a `rospy`_ node:
-        
-        #-------moveit_commander.roscpp_initialize(sys.argv)
-        #-------rospy.init_node("move_group_python_interface_tutorial", anonymous=True)
-
         ## Instantiate a `RobotCommander`_ object. Provides information such as the robot's
         ## kinematic model and the robot's current joint states
         robot = moveit_commander.RobotCommander()
@@ -158,8 +149,8 @@ class MoveGroupPythonInterfaceTutorial(object):
         ## arm planning group.
         ## This interface can be used to plan and execute motions:
         #group_name = "panda_arm"
-        #group_name = "panda_arm"
-        group_name = "panda_manipulator"
+        #group_name = "panda_hand"
+        group_name = "panda_arm"
         move_group = moveit_commander.MoveGroupCommander(group_name)
 
         ## Create a `DisplayTrajectory`_ ROS publisher which is used to display
@@ -170,13 +161,6 @@ class MoveGroupPythonInterfaceTutorial(object):
             queue_size=20,
         )
 
-
-
-        ## END_SUB_TUTORIAL
-
-        ## BEGIN_SUB_TUTORIAL basic_info
-        ##
-        ## Getting Basic Information
         ## ^^^^^^^^^^^^^^^^^^^^^^^^^
         # We can get the name of the reference frame for this robot:
         planning_frame = move_group.get_planning_frame()
@@ -190,13 +174,6 @@ class MoveGroupPythonInterfaceTutorial(object):
         group_names = robot.get_group_names()
         print("============ Available Planning Groups:", robot.get_group_names())
 
-        ## Sometimes for debugging it is useful to print the entire state of the
-        ## robot:
-        #print("============ Printing robot state")
-        #print(robot.get_current_state())
-        #print("")
-        ## END_SUB_TUTORIAL
-
         # Misc variables
         self.box_name = ""
         self.robot = robot
@@ -206,6 +183,13 @@ class MoveGroupPythonInterfaceTutorial(object):
         self.planning_frame = planning_frame
         self.eef_link = eef_link
         self.group_names = group_names
+        self.group_name = group_name
+
+        joint_goal = self.move_group.get_current_joint_values()
+        print("============ Panda Joints:")
+        print(joint_goal)
+        print("---------------------------")
+
 
     def go_to_joint_state(self):
         # Copy class variables to local variables to make the web tutorials more clear.
@@ -213,15 +197,8 @@ class MoveGroupPythonInterfaceTutorial(object):
         # reason not to.
         move_group = self.move_group
 
-        ## BEGIN_SUB_TUTORIAL plan_to_joint_state
-        ##
-        ## Planning to a Joint Goal
-        ## ^^^^^^^^^^^^^^^^^^^^^^^^
-        ## The Panda's zero configuration is at a `singularity <https://www.quora.com/Robotics-What-is-meant-by-kinematic-singularity>`_, so the first
-        ## thing we want to do is move it to a slightly better configuration.
-        ## We use the constant `tau = 2*pi <https://en.wikipedia.org/wiki/Turn_(angle)#Tau_proposals>`_ for convenience:
-        # We get the joint values from the group and change some of the values:
         joint_goal = move_group.get_current_joint_values()
+
 
         joint_goal[0] = 0
         joint_goal[1] = -tau / 8
@@ -249,7 +226,7 @@ class MoveGroupPythonInterfaceTutorial(object):
         ## For testing:
         current_joints = move_group.get_current_joint_values()
 
-        new_arr_pos = np.concatenate( (current_joints, [0, 0] ) )
+        new_arr_pos = np.concatenate( (current_joints, [0.2, 0.2] ) )
         joint_command_isaac.position = new_arr_pos    
         pub.publish(joint_command_isaac)
 
@@ -563,6 +540,38 @@ class MoveGroupPythonInterfaceTutorial(object):
             return self.wait_for_state_update(box_is_known=True, timeout=timeout), box_pose
 
 
+    def add_table_placement(self, timeout=4):
+            # Copy class variables to local variables to make the web tutorials more clear.
+            # In practice, you should use the class variables directly unless you have a good
+            # reason not to.
+            box_name = self.box_name
+            scene = self.scene
+
+            ## BEGIN_SUB_TUTORIAL add_box
+            ##
+            ## Adding Objects to the Planning Scene
+            ## ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+            ## First, we will create a box in the planning scene between the fingers:
+            box_pose = geometry_msgs.msg.PoseStamped()
+            box_pose.header.frame_id = "panda_link0"
+
+
+            box_pose.pose.position.x = -0.5
+            box_pose.pose.position.y = 0
+            box_pose.pose.position.z = 0.025
+
+
+            box_name = "table_placement"
+            scene.add_box(box_name, box_pose, size = (0.5, 0.5, 0.15))
+
+
+            ## END_SUB_TUTORIAL
+            # Copy local variables back to class variables. In practice, you should use the class
+            # variables directly unless you have a good reason not to.
+            self.box_name = box_name
+            return self.wait_for_state_update(box_is_known=True, timeout=timeout), box_pose
+
+
     def add_box(self, timeout=4):
         # Copy class variables to local variables to make the web tutorials more clear.
         # In practice, you should use the class variables directly unless you have a good
@@ -584,7 +593,7 @@ class MoveGroupPythonInterfaceTutorial(object):
         #box_pose.pose.orientation.w = 0.0
 
 
-        box_pose.pose.position.x = 0.4
+        box_pose.pose.position.x = 0.6
         box_pose.pose.position.y = 0
         box_pose.pose.position.z = 0.125
 
@@ -599,17 +608,120 @@ class MoveGroupPythonInterfaceTutorial(object):
         self.box_name = box_name
         return self.wait_for_state_update(box_is_known=True, timeout=timeout), box_pose
 
-    def pick_box(self):
-        current_joints = self.move_group.get_current_joint_values()
 
-        new_arr_pos = np.concatenate( (current_joints, [0.1, 0.1] ) )
-        joint_command_isaac.position = new_arr_pos    
-        pub.publish(joint_command_isaac)
+    def pick_box(self, box):   
+        rospy.sleep(1)
+        rospy.logwarn("moving to test")
+        grasps = [] 
+        0.67611; 0.0091003; 0.71731
+        g = Grasp()
+        g.id = "test"
+        
+        grasp_pose = PoseStamped()
+        grasp_pose.header.frame_id = "panda_link0"
+        grasp_pose.pose.position.x = box.pose.position.x
+        grasp_pose.pose.position.y = box.pose.position.y
+        grasp_pose.pose.position.z = box.pose.position.z + 0.1
 
-        print("Ola mestre")
-        print(new_arr_pos)
+        grasp_pose.pose.orientation.x = 0
+        grasp_pose.pose.orientation.y = 1
+        grasp_pose.pose.orientation.z = 0
+        grasp_pose.pose.orientation.w = 0
 
 
+        rospy.logwarn("moving to arm")
+        move_group = self.move_group
+        
+        rospy.sleep(1)
+        
+        # set the grasp pose
+        g.grasp_pose = grasp_pose
+
+        # define the pre-grasp approach
+        g.pre_grasp_approach.direction.header.frame_id = "panda_link0"
+        g.pre_grasp_approach.direction.vector.x = box.pose.position.x
+        g.pre_grasp_approach.direction.vector.y = box.pose.position.y + 0.2
+        g.pre_grasp_approach.direction.vector.z = -box.pose.position.z - 0.2
+        g.pre_grasp_approach.min_distance = 0.1
+        g.pre_grasp_approach.desired_distance = 0.3
+        g.pre_grasp_posture.header.frame_id = "panda_link0"
+        g.pre_grasp_posture.joint_names = ["panda_finger_joint1", "panda_finger_joint2"]
+    
+        pos = JointTrajectoryPoint()
+        pos.positions.append(0.06)
+    
+        g.pre_grasp_posture.points.append(pos)
+    
+        # set the grasp posture
+        g.grasp_posture.header.frame_id = "panda_link0"
+        g.grasp_posture.joint_names = ["panda_finger_joint1", "panda_finger_joint2"]
+
+        pos = JointTrajectoryPoint()
+        pos.positions.append(0.0)
+        pos.effort.append(0.9)
+    
+        g.grasp_posture.points.append(pos)
+
+        
+        # set the post-grasp retreat
+        g.post_grasp_retreat.direction.header.frame_id = "panda_link0"
+        g.post_grasp_retreat.direction.vector.x = -0.4
+        g.post_grasp_retreat.direction.vector.y = 0.2
+        g.post_grasp_retreat.direction.vector.z = 0.7
+        g.post_grasp_retreat.desired_distance = 0.25
+        g.post_grasp_retreat.min_distance = 0.01
+
+        #g.allowed_touch_objects = ["table", "panda_hand", "Cube"]
+        g.allowed_touch_objects = ["table"]
+        
+
+        g.max_contact_force = 0.5
+        
+        # append the grasp to the list of grasps
+        grasps.append(g)
+        
+    
+        
+        # pick the object
+        #move_group.pick("Cube", grasps)
+        result = False
+        n_attempts = 0
+        
+        
+        ## repeat until will succeed
+        while result == False:
+            print("Attempts pickup: "), n_attempts
+            result = move_group.pick("Cube", grasps)      
+            n_attempts += 1
+            rospy.sleep(1)
+        rospy.loginfo("Pickup successful")
+
+        placement_pose = PoseStamped()
+        placement_pose.header.frame_id = "panda_link0"
+        placement_pose.pose.position.x = -0.5
+        placement_pose.pose.position.y = 0.0
+        placement_pose.pose.position.z = 0.125
+
+        placement_pose.pose.orientation.x = 0
+        placement_pose.pose.orientation.y = 0
+        placement_pose.pose.orientation.z = 0
+        placement_pose.pose.orientation.w = 0
+
+        #move_group.place("Cube", placement_pose)  
+
+        
+        result1 = False
+        n_attempts1 = 0
+        while result1 == False:
+            print("Attempts place: "), n_attempts1
+            result1 = move_group.place("Cube", placement_pose)      
+            n_attempts1 += 1
+            rospy.sleep(0.2)
+        rospy.sleep(1)
+        rospy.loginfo("Placement successful")
+
+        #rospy.spin()
+    
 
     def attach_box(self, timeout=4):
         # Copy class variables to local variables to make the web tutorials more clear.
@@ -687,6 +799,7 @@ class MoveGroupPythonInterfaceTutorial(object):
         )
 
 
+
 def main():
     try:
         print("")
@@ -703,6 +816,7 @@ def main():
         #)
         tutorial = MoveGroupPythonInterfaceTutorial()
         tutorial.add_table()
+        tutorial.add_table_placement()
 
         input("============ Press `Enter` to execute a movement using a joint state goal ...")
         tutorial.go_to_joint_state()
@@ -738,12 +852,12 @@ def main():
         print(end - start)
 
 
-        #input("============ Press `Enter` to add a box to the planning scene ...")
+        input("============ Press `Enter` to add a box to the planning scene ...")
         #tutorial.detach_box()
         #tutorial.remove_box()
         a, box = tutorial.add_box()
-        tutorial.go_to_pose_goal(0.4, 0.0, 0.22)
-        tutorial.pick_box()
+        #tutorial.go_to_pose_goal(0.4, 0.0, 0.22)
+        tutorial.pick_box(box)
 
         #input("============ Press `Enter` to attach a Box to the Panda robot ...")
         #tutorial.attach_box()
