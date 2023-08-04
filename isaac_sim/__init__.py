e1abcba65153028150f7be77b696bde921fe1f2f
filipeRpereira <a12560@alunos.ipca.pt>
2023-08-04 12:58:21 +0100

    1.5.0

---
+++ isaac_sim/__init__.py
@@ -0,0 +1,16 @@
+# Copyright (c) 2021, NVIDIA CORPORATION.  All rights reserved.
+#
+# NVIDIA CORPORATION and its licensors retain all intellectual property
+# and proprietary rights in and to this software, related documentation
+# and any modifications thereto.  Any use, reproduction, disclosure or
+# distribution of this software and related documentation without an express
+# license agreement from NVIDIA CORPORATION is strictly prohibited.
+#
+
+#from omni.isaac.examples.user_examples.warehouse_task_1_manual import WarehouseTask1Manual
+#from omni.isaac.examples.user_examples.warehouse_task_1_manual_extension import WarehouseTask1ManualExtension
+
+from omni.isaac.examples.user_examples.warehouse_task_1_gym import WarehouseTask1Gym
+from omni.isaac.examples.user_examples.warehouse_task_1_gym_extension import WarehouseTask1GymExtension
+
+
